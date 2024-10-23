9531da5491f54ac72f7200b628d23b3f72770a3a
Alan Francis <alan.prime567@gmail.com>
2024-10-23 20:10:46 +0530

    chore:updated date and content

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,22 +1,27 @@
 export const TimeLines = [
-    {
-        date:"01 OCT",
-        description: "Once upon a time, in a world driven by technology, a group of talented designers came together with a shared vision. They believed that design could shape the way people interacted with digital products. With their passion for aesthetics and usability, they founded SquareUp Digital Product Agency's design department. Their mission was to create visually stunning and user-friendly interfaces that would leave a lasting impression."
-    },
-    {
-        date:"02 OCT",
-        description: "Once upon a time, in a world driven by technology, a group of talented designers came together with a shared vision. They believed that design could shape the way people interacted with digital products. With their passion for aesthetics and usability, they founded SquareUp Digital Product Agency's design department. Their mission was to create visually stunning and user-friendly interfaces that would leave a lasting impression."
-    },
-    {
-        date:"03 OCT",
-        description: "Once upon a time, in a world driven by technology, a group of talented designers came together with a shared vision. They believed that design could shape the way people interacted with digital products. With their passion for aesthetics and usability, they founded SquareUp Digital Product Agency's design department. Their mission was to create visually stunning and user-friendly interfaces that would leave a lasting impression."
-    },
-    {
-        date:"04 OCT",
-        description: "Once upon a time, in a world driven by technology, a group of talented designers came together with a shared vision. They believed that design could shape the way people interacted with digital products. With their passion for aesthetics and usability, they founded SquareUp Digital Product Agency's design department. Their mission was to create visually stunning and user-friendly interfaces that would leave a lasting impression."
-    },
-    {
-        date:"05 OCT",
-        description: "Once upon a time, in a world driven by technology, a group of talented designers came together with a shared vision. They believed that design could shape the way people interacted with digital products. With their passion for aesthetics and usability, they founded SquareUp Digital Product Agency's design department. Their mission was to create visually stunning and user-friendly interfaces that would leave a lasting impression."
-    },
-]
+  {
+    date: "22 - 26 OCT",
+    description:
+      "Level 2 tasks are up and set to go! Lock in yourselves for the next round of challenges designed to test your skills to the limit. See you on the other side!",
+  },
+  {
+    date: "30 OCT",
+    description:
+      "What's a competition without a problem-solving challenge right? Launching the ultimate challenge designed to test your wit and grit, taking your skills right to the edge. Ready to prove you got what it takes?",
+  },
+  {
+    date: "6 NOV",
+    description:
+      "All set to level up? It's time to showcase your hardwork and get it reviewed by our expert mentors. Get ready to measure your progress and discover your true potential!",
+  },
+  {
+    date: "12 NOV",
+    description:
+      "Time to collaborate, create and compete! Join forces and put those innovative minds in work together. We're excited to see the brilliance you'll bring to the table!",
+  },
+  {
+    date: "18 NOV",
+    description:
+      "The final showdown is here! The stage is set all ready to bring your projects to life as they get showcased to our distinguished panel of judges. Ready to claim your spot one among the top 100?",
+  },
+];
